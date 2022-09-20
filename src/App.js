dd35898d6e90ed2649897cf618c6791ba6f8ca9e
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-20 19:57:18 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AllRoutes from './Routes/AllRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <h2>hello my frd</h2>
+    <div>
+     <AllRoutes/>
     </div>
   );
 }
