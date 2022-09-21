e196cb208f816d37b77358d1fc3030492db03dcc
kasulaarunteja <kasulaarunteja@gmail.com>
2022-09-21 16:45:24 +0530

    apply

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,78 +1,114 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { useState } from "react";
+import axios from "axios";
 
 const Application = () => {
+  const [app, setApp] = useState({
+    FullName:"",
+    Email:"",
+    Experience:"",
+    Expected_CTC:"",
+    resume:""
+  });
+
+  const handleChange = (e) => {
+    setApp({
+      ...app,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(app);
+    axios
+      .post("https://boom12345.herokuapp.com/apply", app)
+      .then((res) => {
+        console.log(res.app)
+        alert("filled successfull form");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
       <div>
         <Navbar />
       </div>
-      <form className="Auth-form-post">
+      <form className="Auth-form-post" onSubmit={handleClick}>
         <div className="Auth-form-content">
-        <div class="mb-3">
-            <label for="fullname" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="fullname" className="form-label">
               Full Name
             </label>
             <input
               type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
+              className="form-control"
+              id="FullName"
               placeholder="eg: Arun"
+              onChange={handleChange}
             />
           </div>
-          <div class="mb-3">
-            <label for="email" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
               Email
             </label>
             <input
               type="email"
-              class="form-control"
-              id="exampleFormControlInput1"
+              className="form-control"
+              id="Email"
               placeholder="eg: a@gmail.com"
+              onChange={handleChange}
             />
           </div>
-          <div class="mb-3">
-            <label for="experience" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="experience" className="form-label">
               Experience
             </label>
             <input
               type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
+              className="form-control"
+              id="Experience"
               placeholder="eg: 0-10"
+              onChange={handleChange}
             />
           </div>
-          <div class="mb-3">
-            <label for="expectesctc" class="form-label">
-            Expected CTC
+          <div className="mb-3">
+            <label htmlFor="expectesctc" className="form-label">
+              Expected CTC
             </label>
             <input
               type="text"
-              class="form-control"
-              id="exampleFormControlInput1"
+              className="form-control"
+              id="Expected_CTC"
               placeholder="eg: 500000"
+              onChange={handleChange}
             />
           </div>
-          <div class="mb-3">
-            <label for="file" class="form-label">
-            Resume File
+          <div className="mb-3">
+            <label htmlFor="file" className="form-label">
+              Resume File
             </label>
             <input
               type="file"
-              class="form-control"
-              id="exampleFormControlInput1"
+              className="form-control"
+              id="resume"
               placeholder="eg: 500000"
+              onChange={handleChange}
             />
           </div>
-        
+
           <br />
-          <div class="d-grid gap-2 col-6 mx-auto">
-            <button class="btn btn-primary" type="button">
+          <div className="d-grid gap-2 col-6 mx-auto">
+            <button className="btn btn-primary" type="submit">
               Submit
             </button>
           </div>
         </div>
-        </form>
+      </form>
     </div>
   );
 };
